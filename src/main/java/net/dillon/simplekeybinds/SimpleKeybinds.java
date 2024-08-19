d8ad690b62ee80be33a931bad4906c14423c3f2e
Dillon8775 <dilloneystrickland12@gmail.com>
2024-08-19 18:20:23 -0400

    lower minimum brightness to 0.0

--- src/main/java/net/dillon/simplekeybinds/SimpleKeybinds.java
+++ src/main/java/net/dillon/simplekeybinds/SimpleKeybinds.java
@@ -11,7 +11,7 @@ import org.apache.logging.log4j.Logger;
 public class SimpleKeybinds implements ClientModInitializer {
 	public static boolean fog = true;
 	public static boolean fullBright = false;
-	public static final double minBrightness = 0.5D;
+	public static final double minBrightness = 0.0D;
 	public static final double maxBrightness = 12.0D;
 	public static final Logger LOGGER = LogManager.getLogger("Simple Keybinds");
 
