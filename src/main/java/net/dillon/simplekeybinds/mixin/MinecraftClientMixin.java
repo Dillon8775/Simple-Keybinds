9abb91c1f4a5306ca2e761b7c819903352855ec4
Dillon8775 <dilloneystrickland12@gmail.com>
2024-08-29 22:31:31 -0400

    add full compatibility for the speedrunner mod

--- src/main/java/net/dillon/simplekeybinds/mixin/MinecraftClientMixin.java
+++ src/main/java/net/dillon/simplekeybinds/mixin/MinecraftClientMixin.java
@@ -17,8 +17,6 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-import static net.dillon.simplekeybinds.SimpleKeybinds.isSpeedrunnerModLoaded;
-
 @Environment(EnvType.CLIENT)
 @Mixin(MinecraftClient.class)
 public class MinecraftClientMixin {
